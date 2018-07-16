ef101d9f9401760ff00c1daaa9243ac82e455684
myeongsu <oplu92@gmail.com>
2018-07-16 16:27:09 +0900

    20180716 경명수 works_detail 수정

--- src/main/webapp/interia/js/sp_include.js
+++ src/main/webapp/interia/js/sp_include.js
@@ -7,3 +7,8 @@ $(window).scroll(function () {
   	$(".ib-pd-foot").css("display", "flex");
   }
 });
+
+function fnMove(seq){
+    var offset = $("#produ" + seq).offset();
+    $('html, body').animate({scrollTop : offset.top - 120}, 400);
+}
