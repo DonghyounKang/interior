062927b3aca9882bc2b8ab48c0bda71a45c01072
donghyounkang <donghyounkang@gmail.com>
2018-07-17 17:21:37 +0900

    2017.07.17 동현 conflict solve

--- src/main/webapp/interia/js/ad_header.js
+++ src/main/webapp/interia/js/ad_header.js
@@ -1,16 +1,10 @@
-//$(document).ready(function(){
-//    $(".ad-nav-item > a").click(function(){
-//        $(".ad-nav-second").animate({
-//            height: 'toggle'
-//        });
-//    });
-//});
+
 $.get("/interior/interia/html/admin/store_admin_header.html", (data) => {
     $("#ad-header").html(data);
 });
 
 
 
-function fntt() {
-	$(".ad-nav-second").slideToggle("slow");
-};
+//function fntt() {
+//	$(".ad-nav-second").slideToggle("slow");
+//};
