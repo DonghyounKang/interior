79efdfe9d79e48e8e807b06a931e602cdff2741f
donghyounkang <donghyounkang@gmail.com>
2018-07-17 15:25:28 +0900

    2018.07.17 동현 admin sidebar 구현
    ㅁㄴㅇㄹ

--- src/main/webapp/interia/js/ad_header.js
+++ src/main/webapp/interia/js/ad_header.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
-    $(".ad-nav-link").click(function(){
-        $(".ad-nav-second").animate({
-            height: 'toggle'
-        });
-    });
-});
-
+//$(document).ready(function(){
+//    $(".ad-nav-item > a").click(function(){
+//        $(".ad-nav-second").animate({
+//            height: 'toggle'
+//        });
+//    });
+//});
 $.get("/interior/interia/html/admin/store_admin_header.html", (data) => {
     $("#ad-header").html(data);
 });
+
+
+
+function fntt() {
+	$(".ad-nav-second").slideToggle("slow");
+};
