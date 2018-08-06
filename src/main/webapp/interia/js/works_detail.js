1871bb08dc663e07db210c09d93569145b6cb86e
berkleylim <berkleylim16@gmail.com>
2018-08-06 17:17:03 +0900

    2018. 8. 6. (월) - 임현식 (제품상세관련) (아직 미완성, 조금있다 handlebars 응용하여 다시 할 예정)

--- src/main/webapp/interia/js/works_detail.js
+++ src/main/webapp/interia/js/works_detail.js
@@ -1,74 +1,105 @@
 // 제품 판매 관련 값 변경하기
+
+// => 제품번호 추출
 var no = location.href.split("?")[1];
 
-
-// 1. 이미지 부분은 나중에 건들겠음
-
-// 밑에서부터 시작!
 $.getJSON(serverRoot + "/json/works/" + no, (result) => {
-	var title = result.title;
-	var price = result.price;
-	
-	$("#fTitle").text(title);
-	$("#fPrice").text(price);
-	$("#fDeliveryPrice").text("2500");
-	
-	// 배송비 저장
-	var deliveryPrice = 2500;
+	// 1) 제목 수정
+	$("#fTitle").text(result.title);
 	$("#fTitle2").text(result.title);
 	
-	// 재고 설정
-	var maxCapacity = result.capacity;
-	$("#seller-value").text(1);
-	$("#seller-value").val(1);
-	var sellerValue = $("#seller-value").text();
+	// 2) 가격 수정 - 초기화
+	$("#fPrice").text(result.price);
 	
-	// 재고의 따른 가격
-	$("#price-value").text(price * $("#seller-value").text());
+	// 3) 택배비 수정
+	// 배송비 저장
+	var deliveryPrice = 0;
+	if (result.deliveryPrice == "y" || result.deliveryPrice == "Y") {
+		deliveryPrice = 2500;
+		$("#fDeliveryPrice").text(deliveryPrice + " 원");
+		$("#fDeliveryPrice").val(2500);
+	} else {
+		$("#fDeliveryPrice").text("무  료");
+		$("#fDeliveryPrice").val(0);
+	}
 	
-	// 총 상품 금액
-	$("#All-Price").text((sellerValue * price) + deliveryPrice);
-	$("#All-Price").val((sellerValue * price) + deliveryPrice);
+	// 4) 구매 개수 설정 (초기화 : 1)
+	var maxCapacity = result.capacity; // 최대 재고량
+	var sellerValue = 1;  // 판매갯수 (초기값 : 1)
+	$("#seller-value").text(sellerValue);
+	$("#seller-value").val(sellerValue);
 	
-	// 구매 갯수 증가 이벤트
+	
+	// 5) 판매갯수의 따른 가격 설정
+	$("#price-value").text(result.price * sellerValue);
+	
+	// 6) 총 상품 금액
+	$("#All-Price").text((sellerValue * result.price) + deliveryPrice);
+	$("#All-Price").val((sellerValue * result.price) + deliveryPrice);
+	
+	// 7) 구매 갯수 증가 이벤트
 	$("#plus-value").click(() => {
 		if (sellerValue == maxCapacity) {
 			window.alert("더이상의 재고 수량이 존재하지 않습니다.");
 		} else {
-			$("#seller-value").text(sellerValue++);
-			$("#price-value").text(price * $("#seller-value").text());
-			$("#All-Price").text((sellerValue * price) + deliveryPrice);
-			$("#All-Price").val((sellerValue * price) + deliveryPrice);
+			$("#seller-value").text(++sellerValue);
+			$("#price-value").text(result.price * sellerValue);
+			$("#All-Price").text((sellerValue * result.price) + deliveryPrice);
+			$("#All-Price").val((sellerValue * result.price) + deliveryPrice);
 		}
 	});
 	
-	// 구매 갯수 감소 이벤트
+	// 8) 구매 갯수 감소 이벤트
 	$("#minus-value").click(() => {
 		if (sellerValue == 1) {
 			window.alert("최소 1개 이상을 지정해야합니다.");
 		} else {
-			$("#seller-value").text(sellerValue--);
-			$("#price-value").text(price * $("#seller-value").text());
-			$("#All-Price").text((sellerValue * price) + deliveryPrice);
-			$("#All-Price").val((sellerValue * price) + deliveryPrice);
+			$("#seller-value").text(--sellerValue);
+			$("#price-value").text(result.price * sellerValue);
+			$("#All-Price").text((sellerValue * result.price) + deliveryPrice);
+			$("#All-Price").val((sellerValue * result.price) + deliveryPrice);
 		}
 	});
 	
-	//console.log(result);
+	// 메인만 이미지 표시
+	var mainPhoto = "";
+	for (var index in result.worksPhoto) {
+		//console.log(result.worksPhoto[index].mainPhoto);
+		if(result.worksPhoto[index].mainPhoto == "y" 
+			|| result.worksPhoto[index].mainPhoto == "Y") {
+			mainPhoto = result.worksPhoto[index].path;
+			break;
+		}
+	}
+	$("#main-image").attr("src","../../images/works/works_list/" + mainPhoto);
 	
-	// 메인만 이미지 표시 (임시로 테스트용으로 둔 것!)
-	$("#main-image").attr("src","../../images/works/works_list/" + result.photo.path);
+	
+	// 나머지 이미지
 	
 	
+	// 핸들러 제어 - 옵션 속성 값
+	var templateFn = Handlebars.compile($('#AttributeValue-template').html())
+	$(fAttributeValue).html(templateFn({select:result.worksOption}))
 	
-	// 핸들러 제어
-    var trTemplateSrc=$('#tr-template').html();
-    
-    // 위에서 준비한 템플릿 데이터를 가지고 HTML을 생성할 템플릿 엔진 준비
-    var templateFn = Handlebars.compile(trTemplateSrc)
-    
-    $.getJSON("../../../json/works/option/" + no , (data) => {
-        console.log(data);
-        $(fAttributeValue).html(templateFn({select:data}))
-    });
+	// 핸들러 제어 - 옵션 속성 이름
+	templateFn = Handlebars.compile($('#AttributeName-template').html())
+	$(fAttributeName).html(templateFn({select:result.worksOption}))
+	
+	
+
+	// 장바구니 담기 구현
+	$("#btn-basket").click(() => {
+		
+	});
+	
+	// 구매하기 버튼 구현
+	$("#btn-purchased").click(() => {
+	});
+	
 });
+
+
+
+
+
+
