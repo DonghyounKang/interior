df5041c1c05251c0a6fb53b16f83436c9a42e973
sangkyeonglee <sangkyeong.lee93@gmail.com>
2018-08-14 16:31:30 +0900

    2018.08.14 이상경 ( 마이페이지 좋아요 )

---
+++ src/main/webapp/interia/js/mp_like.js
@@ -0,0 +1,48 @@
+$.getJSON("../../../json/mylike/list", (data) => {
+	for (let item of data) {
+		$('<a onclick="mkmodal('+ item.BNO +')" href="#myModal" data-toggle="modal">'+
+			'<div class="ib-mp-post">'+
+				'<img src="../../images/sns/'+ item.PATH +'" alt="게시물">'+
+				'<div class="ib-mp-icons post_like_count'+ item.BNO +'"></div>'+
+			'</div>'+	
+		  '</a>').appendTo('.ib-mp-pgroup');
+		$.getJSON("../../../json/mylike/likeCount"+item.BNO, (data) => {
+			$('<div style="padding-right: 10px;"><i class="fas fa-heart"></i> '+ data.count +'</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;').appendTo(".post_like_count"+ item.BNO);
+			$.getJSON("../../../json/mylike/commentCount"+item.BNO, (data) => {
+				$('<div><i class="fas fa-comment-dots"></i> '+ data.count +'</div>').appendTo(".post_like_count"+ item.BNO);
+			});
+		});
+		
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*끝 */
