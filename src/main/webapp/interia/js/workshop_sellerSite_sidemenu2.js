244534aa0008d31ca8cae5f92ed76bc93fbde6ff
psk041e <tjrud64@gmail.com>
2018-08-19 14:42:46 +0900

    2018.08.19 박서경 공방 이미지 경로 수정

--- src/main/webapp/interia/js/workshop_sellerSite_sidemenu2.js
+++ src/main/webapp/interia/js/workshop_sellerSite_sidemenu2.js
@@ -4,16 +4,16 @@ console.log(no2);
 $.getJSON(serverRoot + "/json/workshop/listSellerSite",{"no":no2}, (data) => {
 	console.log(data);
 	console.log(data[0].lpath);
-	$("<img src='../../../files/" + data[0].lpath + "'>"
+	$("<img src='../../../files/workshop/" + data[0].lpath + "'>"
 	).appendTo(".workshop_sellerSite_sidemenu_img");
 	$("<a href='../admin/store_admin_index.html'>" +
 			"<h1>" + data[0].wsnm + "</h1>" +
 			"</a>" +
 			"<p>" + data[0].itrod + "</p>"
 	).appendTo(".workshop_sellerSite_sidemenu_imgtext");
-	$("<img src='../../../files/" + data[0].path + "'>"
+	$("<img src='../../../files/workshop/" + data[0].path + "'>"
 	).appendTo(".workshop_sellerSite_content_visual");
-	$("<img src='../../../files/" + data[0].lpath + "' style='width: 50px;'>"
+	$("<img src='../../../files/workshop/" + data[0].lpath + "' style='width: 50px;'>"
 	).appendTo(".snsbanner00_title_00");
 	$("<p>" + data[0].wsnm + "</p>"
 	).appendTo(".snsbanner00_title_01");
